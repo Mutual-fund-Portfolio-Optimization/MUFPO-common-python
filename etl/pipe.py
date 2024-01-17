b671693b2a64c1a537b1f4b38c059e3129cb2903
vitvara <gridxlgm@gmail.com>
2024-01-17 16:06:59 +0700

    first commit

---
+++ etl/pipe.py
@@ -0,0 +1,33 @@
+class Pipe:
+    def __init__(self, value, failed=False):
+        self.value = value
+        self.failed = failed
+    def get(self):
+        return self.value
+    def is_failed(self):
+        return self.failed
+    def __str__(self):
+        return ' '.join([str(self.value), str(self.failed)])
+    def bind(self, f, arg=False, kwarg=False):
+        if self.failed:
+            return self
+        try:
+            if arg:
+                x = f(*self.get())
+            elif kwarg:
+                x = f(**self.get())
+            else:
+                x = f(self.get())
+            return Pipe(x)
+        except Exception as err:
+            print(err)
+            return Pipe(None, True)
+    
+    def __rshift__(self, f):
+        return self.bind(f, arg=True)
+    
+    def __gt__(self, f):
+        return self.bind(f, kwarg=True)
+
+    def __or__(self, f):
+        return self.bind(f)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MUFPO common python library',
+    version='1.0',
+    packages=find_packages(),
+    author='Vitvara Varavithya',
+    author_email='grid.vitvara@gmail.com'
+)
