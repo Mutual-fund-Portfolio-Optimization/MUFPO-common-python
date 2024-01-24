30cf94c3fa51fcb4965310363902a7c6704757f2
TaninDean <reiodean.dean@gmail.com>
2024-01-24 18:40:37 +0700

    Fix command error

--- mufpo/database_manager/database_manager.py
+++ mufpo/database_manager/database_manager.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text, Table, Column, Integer, String, MetaData
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-from sqlalchemy import text
 
 class MariaDBManager:
     _instance = None
@@ -30,26 +29,55 @@ class MariaDBManager:
         try:
             with self.engine.connect() as conn:
                 conn.execute(text(f"CREATE DATABASE {db_name}"))
+                conn.commit()
                 print(f"Database {db_name} created successfully")
         except SQLAlchemyError as e:
             print(f"Error: {e}")
+
 
     def delete_database(self, db_name):
         """Delete a database."""
         try:
             with self.engine.connect() as conn:
-                conn.execute(f"DROP DATABASE {db_name}")
+                conn.execute(text(f"DROP DATABASE {db_name}"))
+                conn.commit()
                 print(f"Database {db_name} deleted successfully")
         except SQLAlchemyError as e:
             print(f"Error: {e}")
 
-    def execute_query(self, query, **kwargs):
+    def execute_query(self, db_name, query, **kwargs):
         """Execute a SQL query."""
         try:
             with self.engine.connect() as conn:
+                conn.execute(text(f"USE {db_name}"))
                 conn.execute(text(query), **kwargs)
+                conn.commit()
+                print(f"{query} \n successful.")
         except SQLAlchemyError as e:
             print(f"Error: {e}")
+
+
+    def list_databases(self):
+        """List all databases."""
+        try:
+            with self.engine.connect() as conn:
+                res = conn.execute(text('SHOW DATABASES')).fetchall()
+                return [table[0] for table in res]
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            return []
+
+    def list_tables(self, db_name):
+        """List all tables in a specific database."""
+        try:
+            query = f"SELECT table_name FROM information_schema.tables WHERE table_schema = '{db_name}'"
+            with self.engine.connect() as conn:
+                result = conn.execute(text(query)).fetchall()
+                # Each result is a tuple, so extract the first element to get the table name
+                return [table[0] for table in result]
+        except SQLAlchemyError as e:
+            print(f"Error: {e}")
+            return []
 
     def close_connection(self):
         """Close the database connection."""
